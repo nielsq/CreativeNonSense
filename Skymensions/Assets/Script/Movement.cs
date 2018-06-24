b67597ff4da1b14ec6a1b49068146e1a928d754b
nielsq <nielsquanz@googlemail.com>
2018-06-24 19:13:58 +0200

    Jump
    Springen ist jetzt möglich.
    Die physik ist noch nicht so gut...
    Double Jump funktionert auch.

--- Skymensions/Assets/Script/Movement.cs
+++ Skymensions/Assets/Script/Movement.cs
@@ -2,42 +2,150 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
 
 
     //var
-    public int speed = 1;
-    private Vector3 forward;
+    public float speed = 1.0f;
+    public bool doubleJump = true;
+    public float updates = 0.5f;
 
+    private bool pressJump = true;
+    private bool JumpCoolDown = false;
+    private bool IsGrounded;
+    private Vector3 JumpVector = new Vector3(0.0f, 7.0f, 0.0f);
+    float tempTime;
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKey(KeyCode.W))
-        {
+    void Start()
+    {
 
-            this.transform.Translate(0.0f, 0.0f, 0.3f * speed);
-        }
-
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            this.transform.Translate(-(0.3f * speed), 0.0f, 0.0f);
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            this.transform.Translate(0.0f, 0.0f, -(0.3f * speed));
-        }
-
-        if (Input.GetKey(KeyCode.D) )
-        {
-            this.transform.Translate(0.3f * speed, 0.0f, 0.0f);
-
-        }
     }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        //bewegung mit WASD
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(0.0f, 0.0f, 1.0f * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(-1.0f * speed, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(0.0f, 0.0f, -1.0f * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(1.0f * speed, 0.0f, 0.0f);
+        }
+
+
+        //damit wird die geschwindigkeit verringert falls zwei tasten ( WA/WD SA/SD)  gedrückt werden.
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-0.5f * speed, 0.0f, 0.5f * speed);
+        }
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(0.5f * speed, 0.0f, 0.5f * speed);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-0.5f * speed, 0.0f, -0.5f * speed);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(0.5f * speed, 0.0f, -0.5f * speed);
+        }
+
+
+        //Jump
+
+
+        if (!Input.GetKey(KeyCode.Space))
+        {
+            pressJump = true;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && pressJump)
+        {
+            pressJump = false;
+            if (doubleJump)
+            {
+
+                if (IsGrounded)
+                {
+                    GetComponent<Rigidbody>().AddForce(JumpVector, ForceMode.Impulse);
+                    Debug.Log("jump");
+                } else if (!JumpCoolDown)
+                {
+                    JumpCoolDown = true;
+                    GetComponent<Rigidbody>().AddForce(JumpVector, ForceMode.Impulse);
+                    Debug.Log("double");
+                }
+            }
+            else
+            { 
+                if (IsGrounded)
+                {
+                    GetComponent<Rigidbody>().AddForce(JumpVector, ForceMode.Impulse);
+                }
+                
+            }
+
+            
+        }
+
+
+        tempTime += Time.deltaTime;
+        if (tempTime > updates)
+        {
+            tempTime = 0;
+            UpdateEveryX();
+        }
+
+
+    }
+
+
+
+    void UpdateEveryX()
+    {
+
+        
+
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        IsGrounded = true;
+        JumpCoolDown = false;
+        Debug.Log("isGrounded");
+
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        IsGrounded = false;
+        Debug.Log("not");
+    }
+
+
+
+
+
 }
+
+
+   
+
+
