6ac0be1f3175b8356e15aa1ae87794ca0de88f61
nielsq <nielsquanz@googlemail.com>
2018-08-06 13:08:14 +0200

    bisschen ki
    bisschen ki

---
+++ Skymensions/Assets/KI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KI : MonoBehaviour {
+
+
+    public GameObject spieler;
+
+
+    // Use this for initialization
+    void Start () {
+    
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (spieler.transform.position.x <= this.transform.position.x || spieler.transform.position.x >= this.transform.position.x ) {
+
+        }
+		
+	}
+}
