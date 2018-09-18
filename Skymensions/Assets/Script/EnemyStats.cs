28868b805ca8a7556196bfe5929aebbef54d5319
Hobbobbelmobmob <40360145+Hobbobbelmobmob@users.noreply.github.com>
2018-09-18 15:00:13 +0200

    Player Stats und UI
    Minimalstes UI um die 3 Stats abzubilden. Tests eingefügt: F drücken um einen Schaden zu erhalten, Q drücken um eine Random Armor mit Wert zwischen 1-30 anzulegen.

---
+++ Skymensions/Assets/Script/EnemyStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+
+    //Anfangswerte
+    private int enemyBaseHP = 4;
+    private int enemyBaseATK = 1;
+    private int enemyBaseArmor = 0;
+
+    //playerLevel skaliert mit den Stats der Gegner. Attack = base + playerLevel oder + playerLevel*0.x
+    //RNG um Gegener Art zu ermitteln (Random Number 1 = Spinne, 2 = Bär... etc.)
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
