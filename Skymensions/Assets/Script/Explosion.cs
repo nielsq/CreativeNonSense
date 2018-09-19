aece2e4503d83ff081456bbcebf24ccb7310949a
Daniel J <40360106+greyeZ@users.noreply.github.com>
2018-09-19 20:36:30 +0200

    Explosion Shroom
    So,
    ich hoffe das ich GitHub entlich zum laufen gebracht habe.
    Das ist die erste Falle, ein explodierender Pilz.

---
+++ Skymensions/Assets/Script/Explosion.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+   
+    public float power = 40.0f;
+    public float radius = 5.0f;
+    public float upforce = 1.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    /*void fixedUpdate()
+    {
+        if (bomb == enabled)
+        {
+            Invoke("Detonate", 5);
+        }
+    } */
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            
+                Invoke("Detonate", 3);
+            
+        }
+    }
+
+    void Detonate()
+    {
+        Destroy(gameObject);
+        Debug.Log("Explosion!!!");
+        Vector3 explosionPosition = gameObject.transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(power, explosionPosition, radius, upforce, ForceMode.Impulse);
+            }
+        }
+    }
+    // Update is called once per frame
+  /*  void Update()
+    {
+
+        Vector3 explosionPosition = bomb.transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(power, explosionPosition, radius, upforce, ForceMode.Impulse);
+            }
+        }
+    } */
+}
