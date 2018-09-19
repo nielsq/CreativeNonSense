cbfd78e2a01ec7e4e3952642424e802be1223daf
Daniel J <40360106+greyeZ@users.noreply.github.com>
2018-09-19 23:16:57 +0200

    Respawn
    Hab mal eine Plane eigefügt die das Level Reloaded, muss noch eine GUI und  einfügen und ein paar Bugs fixen.

---
+++ Skymensions/Assets/Script/Respawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Respawn : MonoBehaviour {
+
+    private Scene scene;
+	// Use this for initialization
+	void Start () {
+        scene = SceneManager.GetActiveScene();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Respawnen!!!");
+            Application.LoadLevel(scene.name);
+
+        }
+    }
+
+
+}
