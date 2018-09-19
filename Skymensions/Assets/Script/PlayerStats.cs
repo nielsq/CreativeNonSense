2164a03cc75b8b1c2d32a736f2934e0ed55ff94e
Hobbobbelmobmob <robert.jonetzko@informatik.hs-fulda.de>
2018-09-19 13:56:11 +0200

    Knockback
    by Robert

--- Skymensions/Assets/Script/PlayerStats.cs
+++ Skymensions/Assets/Script/PlayerStats.cs
@@ -5,15 +5,15 @@ using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour {
 
-
-
+    public GameObject enemy;
+    public int abstand = 4;
     //Anfangswerte
     private int playerBaseHP = 10;
     private int playerBaseATK = 2;
     private int playerBaseArmor = 0;
 
     //Aktualisierte Werte
-    public int playerCurrentHP;
+    private int playerCurrentHP;
     private int playerMaxHP;
     private int playerCurrentATK;
     private int playerCurrentArmor;
@@ -40,8 +40,17 @@ public class PlayerStats : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        //TEST -> funktioniert
-        if(Input.GetKeyDown(KeyCode.F))
+        if (enemy.transform.position.x - this.transform.position.x >= -abstand && enemy.transform.position.x - this.transform.position.x <= abstand)
+        {
+
+            if (enemy.transform.position.z - this.transform.position.z >= -abstand && enemy.transform.position.z - this.transform.position.z <= abstand)
+            {
+                TakeDMG(1);
+            }
+        }
+
+                //TEST -> funktioniert
+        if (Input.GetKeyDown(KeyCode.F))
         {
             TakeDMG(1);
         }
@@ -54,14 +63,25 @@ public class PlayerStats : MonoBehaviour {
 
     }
 
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Gegner")
+        {
+            
+        }
+    }*/
+
     void TakeDMG (int dmg)
     {
         playerCurrentHP -= dmg;
         //Evtl noch Lifebar Update
         hpText.text = "Hitpoints: " + playerCurrentHP;
+        //Knockback
+        Vector3 direction = (this.transform.position - enemy.transform.position).normalized;
+        this.GetComponent<Rigidbody>().AddForce(direction * 50, ForceMode.Impulse);
     }
 
-    public void NewArmor (int armor)
+    void NewArmor (int armor)
     {
         playerCurrentArmor = playerBaseArmor + armor;
         armorText.text = "Armor: " + playerCurrentArmor;
@@ -73,10 +93,8 @@ public class PlayerStats : MonoBehaviour {
         playerCurrentATK = playerBaseATK + swordATK;
         atkText.text = "Attack: " + playerCurrentATK;
     }
-
-
-
-    void OnCollisionEnter(Collision col)
+	
+	void OnCollisionEnter(Collision col)
     {
 
 
