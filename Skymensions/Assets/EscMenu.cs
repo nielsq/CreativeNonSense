8eed20e6dbd5265b39dfdcce902e5f6510d4b214
Daniel J <40360106+greyeZ@users.noreply.github.com>
2018-09-20 10:21:16 +0200

    EscMenu
    Wenn man auf Escape drückt, sollte jetzt auch ein Menü kommen.
    Bug: Spiel läuft im Hintergrund noch weiter.

---
+++ Skymensions/Assets/EscMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscMenu : MonoBehaviour {
+
+    // Use this for initialization
+    public static bool GameIsPaused = false;
+
+    public GameObject escMenuUI;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }else
+            {
+                Pause();
+            }
+        }
+	}
+
+   public void Resume()
+    {
+        escMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        escMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+     
+    }
+}
