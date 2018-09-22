6858dffc5458a3a491b834973950bf5b14d67c3d
Hobbobbelmobmob <40360145+Hobbobbelmobmob@users.noreply.github.com>
2018-09-22 14:53:30 +0200

    Spawn angepasst
    - Es sollte keine Gegenstände mehr ineinander spawnen
    - Mushroom Trap zum Random Spawn hinzugefügt (0 - 3 Shrooms werden platziert)

--- Skymensions/Assets/Script/Spawn.cs
+++ Skymensions/Assets/Script/Spawn.cs
@@ -8,9 +8,11 @@ public class Spawn : MonoBehaviour {
     public GameObject tree;
     public GameObject rock;
     public GameObject wall;
+    public GameObject shroom;
     public float posYTree = 7.94f;
     public float posYRock = 3.8f;
     public float posYWall = 7.77f;
+    public float posYShroom = 3.09f;
 
     // Use this for initialization
     void Start () {
@@ -35,6 +37,13 @@ public class Spawn : MonoBehaviour {
             spawnWall();
         }
 
+        //0-3 Pilze spawnen (Random Number)
+        int randomShroom = Random.Range(0, 3);
+        for (int i = 0; i <= randomWall; i++)
+        {
+            spawnShroom();
+        }
+
     }
 	
 	// Update is called once per frame
@@ -46,20 +55,44 @@ public class Spawn : MonoBehaviour {
     void spawnTree()
     {
         Vector3 pos = new Vector3(Random.Range(-30, 30), posYTree, Random.Range(-30, 30));
+        //CheckSphere pos, Radius, ignored Layer -> Ground auf Layer 9 gesetzt, sonst Endlosschleife
+        while (Physics.CheckSphere(pos, 2, 9)) {
+            pos = new Vector3(Random.Range(-30, 30), posYTree, Random.Range(-30, 30));
+        }
         Instantiate(tree, pos, Quaternion.identity);
     }
     //Steine an einer Random Position (auf der Flaeche) spawnen
     void spawnRock()
     {
         Vector3 pos = new Vector3(Random.Range(-30, 30), posYRock, Random.Range(-30, 30));
-        Instantiate(rock, pos, Quaternion.identity);
+        while (Physics.CheckSphere(pos, 2, 9))
+        {
+            pos = new Vector3(Random.Range(-30, 30), posYRock, Random.Range(-30, 30));
+        }
+        Instantiate(rock, pos, Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f));
     }
     
     //Waende an einer Random Position (auf der Flaeche) spawnen
     void spawnWall()
     {
         Vector3 pos = new Vector3(Random.Range(-30, 30), posYWall, Random.Range(-30, 30));
+        while (Physics.CheckSphere(pos, 2, 9))
+        {
+            pos = new Vector3(Random.Range(-30, 30), posYWall, Random.Range(-30, 30));
+        }
         Instantiate(wall, pos, Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f));
      
+    }
+
+    //Pilze an einer Random Position (auf der Flaeche) spawnen
+    void spawnShroom()
+    {
+        Vector3 pos = new Vector3(Random.Range(-30, 30), posYShroom, Random.Range(-30, 30));
+        while (Physics.CheckSphere(pos, 2, 9))
+        {
+            pos = new Vector3(Random.Range(-30, 30), posYShroom, Random.Range(-30, 30));
+        }
+        Instantiate(shroom, pos, Quaternion.identity);
+
     }
 }
