8600e0d75b68747627599219b26a267983b2deeb
Daniel J <40360106+greyeZ@users.noreply.github.com>
2018-09-22 20:30:15 +0200

    Ich hasse GitHub
    Noch push, da GitHub ja nichts spiechert

--- Skymensions/Assets/Script/Gate.cs
+++ Skymensions/Assets/Script/Gate.cs
@@ -26,9 +26,13 @@ public class Gate : MonoBehaviour {
     {
         if (other.tag == "Player")
         {
-
+            scene = SceneManager.GetActiveScene();
             Debug.Log("New Level!!");
             Player = GameObject.FindGameObjectWithTag("Player");
+            Application.LoadLevel(scene.name);
+           
+          
+            Debug.Log("Jetzt die Stats anpassen:" + currAr + currATk + currHP);
             currAr = Player.GetComponent<PlayerStats>().currArmor();
             currATk = Player.GetComponent<PlayerStats>().currAttack();
             currHP = Player.GetComponent<PlayerStats>().currHP();
